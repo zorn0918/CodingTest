cfe5aee603f1acd1dd96859c5850068e2cf0051e
Minjae Lee <parousia0918@gmail.com>
2024-07-09 23:26:24 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int oddSum = 0;
+        int evenSum = 0;
+
+        for (int i = 0; i < num_list.length; i++) {
+            if ((i + 1) % 2 == 1) {
+                oddSum += num_list[i];
+            } else {
+                evenSum += num_list[i];
+            }
+        }
+
+        return Math.max(oddSum, evenSum);
+    }
+}
