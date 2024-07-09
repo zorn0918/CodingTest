b2f75bbc22fde272f3882c217162049e9d5ff9ca
Minjae Lee <parousia0918@gmail.com>
2024-07-09 23:16:15 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 1.80 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+        
+        for (int i = 0; i < queries.length; i++) {
+            int s = queries[i][0];
+            int e = queries[i][1];
+            int k = queries[i][2];
+            
+            int min = Integer.MAX_VALUE;
+            
+            for (int j = s; j <= e; j++) {
+                if (arr[j] > k && arr[j] < min) {
+                    min = arr[j];
+                }
+            }
+            
+            if (min == Integer.MAX_VALUE) {
+                answer[i] = -1;
+            } else {
+                answer[i] = min;
+            }
+        }
+        
+        return answer;
+    }
+}
