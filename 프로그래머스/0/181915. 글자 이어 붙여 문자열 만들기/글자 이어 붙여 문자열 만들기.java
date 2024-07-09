72b7ca74a8a8ec2b0be0c9947c97069819ea608a
Minjae Lee <parousia0918@gmail.com>
2024-07-09 23:17:40 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 0.03 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string, int[] index_list) {
+        StringBuilder answer = new StringBuilder();
+        
+        for (int i : index_list) {
+            answer.append(my_string.charAt(i));
+        }
+        
+        return answer.toString();
+    }
+}
