e6d05be493a53dcd13196e78701b745197231851
Minjae Lee <parousia0918@gmail.com>
2024-07-09 23:32:47 +0900

    [level 3] Title: 거스름돈, Time: 11.39 ms, Memory: 53.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n, int[] money) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+
+        for (int coin : money) {
+            for (int i = coin; i <= n; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        
+        return dp[n] % 1000000007;
+    }
+}
