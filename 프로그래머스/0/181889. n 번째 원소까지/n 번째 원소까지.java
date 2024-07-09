99568b1f62aed6737003dd18c3f37d32c5572725
Minjae Lee <parousia0918@gmail.com>
2024-07-09 23:25:49 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.02 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] result = new int[n];
+        System.arraycopy(num_list, 0, result, 0, n);
+        
+        return result;
+    }
+}
