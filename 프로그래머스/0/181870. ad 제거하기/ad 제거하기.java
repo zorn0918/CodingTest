74c78c133d74bb60ce3db5909a945a0b1cf64817
Minjae Lee <parousia0918@gmail.com>
2024-07-13 22:56:08 +0900

    [level 0] Title: ad 제거하기, Time: 1.15 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public String[] solution(String[] strArr) {
+        List<String> answer = new ArrayList<>();
+        
+        for (String str : strArr) {
+            if (!str.contains("ad")) {
+                answer.add(str);
+            }
+        }
+        
+        return answer.toArray(new String[0]);
+    }
+}
